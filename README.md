2270a3149d5df663e62759b724f37193646db59b
Felipe Matheus Flohr <felipe.flohr@ambisis.com.br>
2022-12-14 17:23:54 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mobile Onboarding -> Clean Architecture
+Este repositório contém os estudos relacionados ao "Clean Architecture", do Uncle Bob.
---
+++ android/app/src/main/kotlin/com/example/mobile_onboarding_clean_arch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_onboarding_clean_arch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
